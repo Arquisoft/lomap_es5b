1480c6fd14aec0e030511a4c63315d1352141326
UO271572 <uo271572@uniovi.es>
2023-03-08 15:05:11 +0100

    añadidos iconos y empezando con css

--- webapp/src/components/Login.tsx
+++ webapp/src/components/Login.tsx
@@ -1,25 +1,40 @@
-import { Button, TextField } from '@mui/material';
+import { Button, Icon, TextField } from '@mui/material';
 import React from 'react';
 import { ReactDOM } from 'react';
+import './Login.css';
+import { Lock } from '@material-ui/icons';
+import { AccountCircle } from '@mui/icons-material';
 //importar css
 
 
-function Login():JSX.Element{
-    return(
-        <>
-            <form className='login-form'>
-                <TextField required
-                    name="username"
-                    label='Name'
-                />
-                <TextField required
-                    name="password"
-                    label='Password'
-                    type={'password'}
-                />
-                <Button type="submit">Login</Button>
-            </form>
-        </>
+function Login(): JSX.Element {
+    return (
+        
+        <form className='login-form' name='login-form'>
+            {/* añadir el logo aqui */}
+            <image name='login-logo' href='' />
+            <h1>Inicia sesion</h1>
+            <div className='inputText'>
+            <AccountCircle></AccountCircle>
+            <TextField required
+                name="username"
+                label='User'
+            />
+            
+            </div>
+            <div className='inputText'>
+            <Lock></Lock>
+            <TextField required
+                name="password"
+                label='Password'
+                type={'password'}
+            />
+            </div>
+            {/*Implementar las acciones de los botones*/}
+            <Button name='login-button' className='login-button' type="submit">Login</Button>
+            <Button name='signup-button' className='signup-button' type="submit">Signup</Button>
+        </form>
+
     );
 }
 
